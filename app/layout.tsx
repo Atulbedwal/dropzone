e5323dcf0bf3c021e88c6ced3447d82e587dbf85
Atulbedwal <atulbedwal69@gmail.com>
2025-07-07 02:36:48 +0530

    added google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,40 @@
-import './globals.css'
-import { Poppins } from 'next/font/google'
+import "./globals.css";
+import { Poppins } from "next/font/google";
+import Script from "next/script";
 
 const poppins = Poppins({
-  subsets: ['latin'],
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-  variable: '--font-poppins',
-})
-
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-poppins",
+});
 
 export const metadata = {
-  title: 'DropZone',
-  description: 'DropZone: Where Your Files Find a Safe Landing!',
-}
+  title: "DropZone",
+  description: "DropZone: Where Your Files Find a Safe Landing!",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={`${poppins.variable} font-poppins antialiased`}
-      >{children}</body>
+      <body className={`${poppins.variable} font-poppins antialiased`}>
+        {children}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-WN36SMPNNY"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-WN36SMPNNY');
+          `}
+        </Script>
+      </body>
     </html>
-  )
+  );
 }
